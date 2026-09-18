afdda077c2d61f65ea3ba247a9490bd0ec0a6eb3
sujunprodhan <144489247+sujunprodhan@users.noreply.github.com>
2026-09-18 16:13:31 +0600

    all check done

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,8 +1,12 @@
 import { useEffect, useState } from 'react';
 import MovieCard from './MovieCard';
 import { Search } from 'lucide-react';
+import { Link, useLocation } from 'react-router';
 
 const Movies = () => {
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
+  
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -23,6 +27,7 @@ const Movies = () => {
         const data = await movieData.json();
         
         if (searchQuery) {
+
           setMovies(data.map(item => item.show));
         } else {
           setMovies(data);
@@ -34,7 +39,6 @@ const Movies = () => {
         setLoading(false);
       }
     };
-
     const debounceTimer = setTimeout(() => {
       fetchData();
     }, 500);
@@ -52,36 +56,44 @@ const Movies = () => {
   if (error) return <p className="text-center text-red-500 mt-10">Error: {error}</p>;
 
   return (
-    <div className="bg-slate-900 min-h-screen py-12">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        
-        {/* Search Bar */}
-        <div className="max-w-xl mx-auto mb-10 relative">
-          <div className="absolute inset-y-0 left-0 pl-4 flex items-center pointer-events-none">
-            <Search className="h-6 w-6 text-gray-400" />
+    <div>
+      {!isHomePage && (
+        <div className="relative bg-slate-900 overflow-hidden">
+          <div className="absolute inset-0">
+            <div className="absolute inset-0 bg-linear-to-r from-slate-900 via-slate-900/80 to-transparent z-10" />
+            <img
+              src="moviebackground.png"
+              alt=""
+              className="w-full h-full object-cover object-center opacity-40"
+            />
           </div>
-          <input
-            type="text"
-            className="block w-full pl-12 pr-4 py-4 border border-slate-700 rounded-2xl leading-5 bg-slate-800 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500 sm:text-lg transition duration-200 ease-in-out shadow-lg"
-            placeholder="Search for a movie..."
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-          />
-        </div>
 
-        <h2 className="text-3xl font-extrabold text-white mb-8 text-center sm:text-left">
-          {searchQuery ? 'Search Results' : 'Popular Shows'}
-        </h2>
-        
-        {movies.length === 0 && !loading && !error ? (
-          <p className="text-center text-gray-400 text-xl mt-10">No movies found matching "{searchQuery}"</p>
-        ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-            {movies.slice(0, 20).map((movie) => (
-              <MovieCard key={movie.id} movie={movie} />
-            ))}
+          {/* Content */}
+          <div className="relative z-20 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-32 lg:py-48 flex justify-center items-center text-center min-h-[80vh]">
+            <h1 className="text-4xl sm:text-5xl lg:text-7xl font-extrabold tracking-tight leading-tight max-w-3xl text-transparent bg-clip-text bg-linear-to-r from-blue-400 to-emerald-400 mx-auto">
+            Favorite Movie
+            </h1>
           </div>
-        )}
+        </div>
+      )}
+      <div className="bg-slate-900 min-h-screen py-12">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <h2 className="text-3xl font-extrabold text-white mb-8 text-center sm:text-left">
+            Popular Shows
+          </h2>
+
+          {movies.length === 0 && !loading && !error ? (
+            <p className="text-center text-gray-400 text-xl mt-10">
+              No movies found matching "{searchQuery}"
+            </p>
+          ) : (
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+              {movies.slice(0, 20).map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
